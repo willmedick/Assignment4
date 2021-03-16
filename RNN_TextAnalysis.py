739221e4c741a9fb8ff9dd45a9dd8f870c7e3be5
floodj21 <56000252+floodj21@users.noreply.github.com>
2021-03-15 21:48:22 -0400

    Add files via upload
    there is some messy stuff in the middle but it is getting there.

--- RNN_TextAnalysis.py
+++ RNN_TextAnalysis.py
@@ -9,51 +9,59 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
 #Read in csv file into pandas data frame
-df = pd.read_csv ('consumer_complaints.csv', usecols=['product','sub_product','issue','sub_issue'])
+df = pd.read_csv ('consumer_complaints.csv', usecols=['product','consumer_complaint_narrative'])
 #Extract complaints and products into a new dataframe
 
 #Check how many values are null and count them
-nullValues = df['sub_product'].isna().sum()
+nullValues = df['consumer_complaint_narrative'].isna().sum()
 print("Number of null values: " + str(nullValues))
 
 #Check how many product values we have
-productValues = df['sub_product'].notna().sum()
+productValues = df['consumer_complaint_narrative'].notna().sum()
 print("Number of product values: " + str(productValues))
 
 #Drop all empty values, reset the index, and drop them
-drop = df.dropna(subset=['sub_product'])
+drop = df.dropna(subset=['consumer_complaint_narrative'])
 newdf0 = drop.reset_index(drop=True, inplace=False)
 #newdf = newdf0.drop(columns=['index'])
 print(newdf0)
 #Recount product values
-productValues = df['sub_product'].notna().sum()
+productValues = df['consumer_complaint_narrative'].notna().sum()
 print("Number of product values: " + str(productValues))
-
 #Create a clean text function
-
+def cleanText(text):
+    
     #Lower case the text
-    
+    text = text.lower()
     #Compile pattern to remove all other characters
-    
+    pattern = re.compile(r"[,.\"!@#$%^&*(){}?/;`~:<>+=-]")
     #Sub the regular expresion with a "" character.
-    
+    text = re.sub(pattern, "", text)
     #Remove x from the text characters with a "" character.
-    
+    text = re.sub('x', "", text)
     #Split the text
-    
+    text = re.split(r'\W+', text)
     #For each word check if its a word and its an alphanumeric
+    #text = re.sub('\w', "", text)
     
     #Remove all english stop words
-    
+    stop_words = set(stopwords.words("english"))
+
+    #text = re.sub(stop_words, "", text)
+    #text = re.sub(stopwords, "", text)
     #Check if each word in the text and add the ones not in stop words
-    
+    return text
     #Join all the text by " "
     
     #Return the clean text
     
-
+newdf0["consumer_complaint_narrative"] = newdf0["consumer_complaint_narrative"].apply(cleanText)
 #Apply clean text to the complaints
+def main():
+    print(newdf0)
 
+if __name__ == "__main__":
+    main()
 
 #Define maximum number of words in our vocabulary to 50000
 
