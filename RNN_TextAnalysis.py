7becbfcbc6dc08f506092cb215437fb156cd775f
floodj21 <56000252+floodj21@users.noreply.github.com>
2021-03-24 17:23:16 -0400

    Add files via upload
    started working on implementing API stuff.

--- RNN_TextAnalysis.py
+++ RNN_TextAnalysis.py
@@ -1,8 +1,22 @@
+"""
+Authors: Jacob Flood and Will Medick
+Name: RNN_TextAnalysis
+Date: 3/22/21
+"""
 import numpy as np 
 import pandas as pd 
 import matplotlib.pyplot as plt
 import re
 import nltk
+import tensorflow as tf
+import tensorflow.keras
+from tensorflow.keras import Sequential
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import SpatialDropout1D
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.callbacks import EarlyStopping
 from collections import Counter
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
@@ -88,10 +102,47 @@ word_index = tf.word_index
 print("Number of unique tokens: ", len(tf.word_counts))
 #Get a text to sequences representation of the complaints
 sequences= tf.texts_to_sequences(newdf0["consumer_complaint_narrative"])
+#sequences= tf.text_to_word_sequence(newdf0["consumer_complaint_narrative"])
+
 #Pad the sequences with the max length
 data = pad_sequences(sequences, maxlen = maxCom, padding = 'post')
 #Print the shape of the data
 print(data.shape)
 #Print the first example of the tokenizer object to the sequences to text
-print("First example: ", data[0])
+tokens = tf.sequences_to_texts(data)
+print("First Example: " , tokens[0])
+#print("First example: ", data[0])
+
+#Get dummy representation of complaints into their one hot encoded vector categories
+dummyList = []
+for item in tokens:
+    dummyList.append(tensorflow.keras.preprocessing.text.one_hot(item, maxWords))
+
+#Split data into training and testing data with a test size of 10%
+X_train, X_test, y_train, y_test = train_test_split(dummyList, test_size=0.10)
+#Create your RNN model
+model = Sequential()
+#Add your embedding layers
+model.add(Embedding(50,000, 250))
+#Add your spatial dropout (20%)
+model.add(SpatialDropout1D(.20))
+#Add your LSTM layer
+model.add(LSTM(100, dropout=.20, recurrent_dropout=.20))
+#Add your dense layer
+model.add(Dense(11, activation='softmax'))
+#Compile your model
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=[tf.keras.metrics.Accuracy()])
+#Define epochs
+epochs = 5
+#Define batch size
+batchSize = 64
+#Fit the model with early stop training
+model.fit(X_train, y_train, batch_size=batchSize, epochs=epochs, callbacks=EarlyStopping(monitor='loss', patience=3), min_delta=0.0001)
+#Evaluate the model for accuracy
+accuracy = tf.keras.metrics.Accuracy.result()
+#Print the accuracy
+print(accuracy)
+#Show graph of loss over time for training data
+
+#Show graph of accuracy over time for training data
 
